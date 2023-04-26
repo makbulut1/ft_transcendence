879e504fa3b46a70d3ceb320843537db63bfb233
Mehmet Eren Akbulut <makbulut@192.168.1.26>
2023-04-26 22:41:29 +0300

    .env file content has been changed

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -8,19 +8,18 @@ import { config } from 'dotenv';
 export class AppService {
   async getHello(query: any): Promise<string> {
     config();
-    const authData: any = await axios.post(config().parsed.TokenURL, {
-      grant_type: config().parsed.GrantType,
+    const authData: any = await axios.post('https://api.intra.42.fr/oauth/token', {
+      grant_type: 'authorization_code',
       code : query.code,
       client_id : config().parsed.ClientID,
       client_secret: config().parsed.SecretID,
       redirect_uri : config().parsed.RedirectURL,
     });
-    const user = await axios.get(config().parsed.UserMeURL, {
+    const user = await axios.get('https://api.intra.42.fr/v2/me', {
       headers: {
         Authorization: `Bearer ${authData?.data?.access_token}`,
       },
     });
-    console.log(user.data.email);
     return 'Hello World!';
   }
 }
