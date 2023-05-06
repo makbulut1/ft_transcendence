6451df4a8d06924505688e0138abb2c6a70d33ac
saozcan <samimozcan@outlook.com>
2023-05-06 13:14:08 +0300

    token from be

--- app/frontend/src/pages/callback.tsx
+++ app/frontend/src/pages/callback.tsx
@@ -5,30 +5,38 @@ import { useEffect, useState } from 'react'
 import { Header } from '@/layouts'
 
 const sendCode = async (code: string) => {
-  const authData: any = await axios.post('https://api.intra.42.fr/oauth/token', {
-    grant_type: 'authorization_code',
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
+
+  //  const authData: any = await axios.post('https://api.intra.42.fr/oauth/token', {
+  //   grant_type: 'authorization_code',
+  //   client_id: "u-s4t2ud-2bfb79667cee369f84ff6967104ee61277be6f58cedeb832183a259967e47f11",
+  //   client_secret: "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",
+  //   code: code,
+  //   redirect_uri: "http://localhost:3000/callback",
+  // })
+  // console.log('authData', authData)
+
+  const postCode = await axios.post('callback', {
     code: code,
-    redirect_uri: process.env.REDIRECT_URI,
   })
-  console.log('authData', authData)
-  const user = await axios.get('https://api.intra.42.fr/v2/me', {
-    headers: {
-      Authorization: `Bearer ${authData?.data?.access_token}`,
-    },
-  })
-  console.log('user', user)
-  const returnUser = await axios.post('callback', {
-    id: user?.data?.id,
-    token: authData?.data?.access_token,
-    fullName: user?.data?.usual_full_name,
-    email: user?.data?.email,
-    login: user?.data?.login,
-  })
-  localStorage.setItem("user", JSON.stringify(returnUser.data))
-  window.location.reload()
-  return returnUser
+  console.log('postCode', postCode)
+
+  // const user = await axios.get('https://api.intra.42.fr/v2/me', {
+  //   headers: {
+  //     Authorization: `Bearer ${authData?.data?.access_token}`,
+  //   },
+  // })
+  // console.log('user', user)
+  // const returnUser = await axios.post('callback', {
+  //   id: user?.data?.id,
+  //   token: authData?.data?.access_token,
+  //   fullName: user?.data?.usual_full_name,
+  //   email: user?.data?.email,
+  //   login: user?.data?.login,
+  // })
+  // localStorage.setItem("user", JSON.stringify(returnUser.data))
+  // window.location.reload()
+  // return returnUser
+  return "ret"
 }
 
 const CallbackPage = () => {
